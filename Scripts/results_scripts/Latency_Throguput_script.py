046619a8ffd3ccdbd94286a3bb1b85203248e955
siavooshpayandehazad <siavooshpayandeh@gmail.com>
2018-01-09 15:41:53 +0200

    documentation for latency/throughput script

--- Scripts/results_scripts/Latency_Throguput_script.py
+++ Scripts/results_scripts/Latency_Throguput_script.py
@@ -1,9 +1,23 @@
+# Copyright (C) 2016 Siavoosh Payandeh Azad
 import os
+
+# This program runs the simulate script for different Packet Injection Rates
+# defined by user in pir_list for dimentions listed in noc_size_list and with
+# packet sizes listed in packet_size_list.
+# please note that the NI depth "NI_depth" has direct impact on the latency
+# results.
+# the results will be dumped in two files
+#       * latency_result.txt
+#       * throughput_result.txt
+
 pir_list = [0.005, 0.01, 0.015, 0.02, 0.025, 0.03]
-noc_size_list = [16]
+noc_size_list = [4, 8]
 packet_size_list = [4, 8, 16]
+# make sure this is equal to your testbench clock period
 clock_duration = 10.0 #ns
-NI_depth = 32 # this is the depth of the NI FIFO. for better results increase it to 128
+# this is the depth of the NI FIFO. for better results
+# increase it to 128 or higher
+NI_depth = 32
 
 latency_result_file = f = open('latency_result.txt', 'w')
 throughput_result_file = f = open('throughput_result.txt', 'w')
